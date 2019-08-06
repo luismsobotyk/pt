f471bef1f5d3b72039c64981dbf34724d75debcc
Alcione <alcionedemorais@gmail.com>
2019-08-06 18:09:36 -0300

    ajustes alcione

---
+++ app/WorkPlans.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WorkPlans extends Model
+{
+    //
+    WorkPlans::all();
+    // Retorna todos os registros da tabela WorkPlans (Seria o equivalente a
+    // SELECT * FROM WorkPlans, para o MySQL)
+
+    WorkPlans::find($id);
+    // Retorna todos os dados do registro da tabela WorkPlans com o $id
+    // especificado na busca
+
+    WorkPlans::where('WorkPlans_id',1)->get();
+    // Executa uma query com parâmetros de restrição (WHERE)
+
+    WorkPlans::where('WorkPlans_id',1)
+    ->orderBy('WorkPlans_id','description')
+    ->take(10)
+    ->get();
+    // Executa uma query com parâmetros de restrição (WHERE e LIMIT)
+    // organizando pelas colunas especificadas
+}
